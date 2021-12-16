09263b53216e729072317ca94f8561186ee01ff4
sharath-nair <38074696+sharath-nair@users.noreply.github.com>
2021-12-16 18:47:50 +0530

    Maturity Selection update. Sort dropdown added. Sorting based on Liquidity and Maturity.

--- src/Modal/MaturityList/Sorting.elm
+++ src/Modal/MaturityList/Sorting.elm
@@ -1,6 +1,16 @@
-module Modal.MaturityList.Sorting exposing (Sorting(..))
+module Modal.MaturityList.Sorting exposing (Sorting(..), toString)
 
 
 type Sorting
     = Liquidity
     | Maturity
+
+
+toString : Sorting -> String
+toString sorting =
+    case sorting of
+        Liquidity ->
+            "Liquidity"
+
+        Maturity ->
+            "Maturity"
