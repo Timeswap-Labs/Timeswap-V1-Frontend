922f123c4f032bcf8542729f104d8eeccc3e8f3f
sharath-nair <38074696+sharath-nair@users.noreply.github.com>
2021-12-24 17:16:18 +0530

    Loading app without metamask

--- src/Wrapper/wallet.ts
+++ src/Wrapper/wallet.ts
@@ -2,6 +2,6 @@ import { Web3Provider } from "@ethersproject/providers";
 
 export declare let window: any;
 
-export const wallet: Wallet = { metamask: new Web3Provider(window.ethereum) };
+export const wallet: Wallet = { metamask: window.ethereum ? new Web3Provider(window.ethereum) : null };
 
-type Wallet = { [name: string]: Web3Provider };
+type Wallet = { [name: string]: Web3Provider | null };
